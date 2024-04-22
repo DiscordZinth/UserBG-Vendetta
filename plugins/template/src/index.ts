4730a5dcd8af31901210f0e8a00323ccbc2c741a
DiscordZinth <167724710+DiscordZinth@users.noreply.github.com>
2024-04-22 14:56:18 +0800

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,29 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { React, url } from "@vendetta/metro/common"
+import { getAssetIDByName } from "@vendetta/ui/assets"
+import { Forms, General } from "@vendetta/ui/components"
+import { showToast } from "@vendetta/ui/toasts"
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+import { fetchData } from "./index"
+
+const { ScrollView } = General
+const { FormSection, FormRow } = Forms
+
+export default () => (<ScrollView>
+    <FormSection>
+        <FormRow
+            label="Discord Server"
+            leading={<FormRow.Icon source={getAssetIDByName("Discord")} />}
+            trailing={FormRow.Arrow}
+            onPress={() => url.openDeeplink("https://discord.gg/TeRQEPb")}
+        />
+        <FormRow
+            label="Reload DB"
+            leading={<FormRow.Icon source={getAssetIDByName("ic_message_retry")} />}
+            onPress={async () => {
+                const fetch = await fetchData()
+                if (!fetch) return showToast("Failed to reload DB", getAssetIDByName("small"))
+                return showToast("Reloaded DB", getAssetIDByName("check"))
+            }}
+        />
+    </FormSection>
+</ScrollView>)
